4a70456e8e86aa56807138f40662d71871c41cba
Magnus Chr. Bareid <magnucb@astro.uio.no>
2016-10-20 16:43:11 +0200

    makedata updated, check with cpp cmd line args

--- python/makedata.py
+++ python/makedata.py
@@ -5,7 +5,7 @@ def make(address):
     """
     initializes the data
     """
-    os.system(str(address)+"/release/Solarsystem.exe") # fitted for my windows dir, hopefully going to get that fixed by today so that directory address applies to linux as well
+    os.system(str(address)+"/release/Solarsystem.exe %s %s" % (sys.argv[1], sys.argv[2])) # fitted for my windows dir, hopefully going to get that fixed by today so that directory address applies to linux as well
 
 if __name__ == '__main__':
     address = os.getcwd()+"/../build-SolarSystem-Desktop_Qt_5_7_0_MSVC2015_64bit-Release"
