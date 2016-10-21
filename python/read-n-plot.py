7cbbdd9892a36fa4b6b589fff7a9ed67d6ba2f37
Magnus Chr. Bareid <magnucb@astro.uio.no>
2016-10-21 16:06:00 +0200

    some more data prints and plot edits

--- python/read-n-plot.py
+++ python/read-n-plot.py
@@ -13,7 +13,7 @@ def read(address):
     bodies_positions = {}
 
     ###### REMEMBER TO EDIT THIS WHEN PLOTTING DIFFERENT STUFF
-    with open(address+"/positions_verlet_SunEarthMercuryVenusMarsJupiterSaturnUranusNeptunePluto.xyz", 'r') as infile:
+    with open(address+"/positions_verlet_SunEarth.xyz", 'r') as infile:
         ###### REMEMBER TO EDIT THIS WHEN PLOTTING DIFFERENT STUFF
         data = infile.read()
     
@@ -56,7 +56,7 @@ def plot(keys, bodies_with_positions):
     ax.set_xlabel('X-axis [AU]')
     ax.set_ylabel('Y-axis [AU]')
     ax.set_zlabel('Z-axis [AU]')
-    ax.set_title(r'Solar system, Euler, $10^5$ steps') ###### REMEMBER TO EDIT THIS WHEN PLOTTING DIFFERENT STUFF
+    ax.set_title(r'Sun+Earth, Verlet, $10^3$ steps') ###### REMEMBER TO EDIT THIS WHEN PLOTTING DIFFERENT STUFF
     ax.legend(loc='lower left')
     pl.show()
     body_string = ""
