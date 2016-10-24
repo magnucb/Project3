c9af635f79d61037c5384ad91a0abf3597b1948c
oyvind <oyvind.svendsen1509@gmail.com>
2016-10-24 23:26:55 +0200

    finished perihelion method in c++program

--- Project_Sol/main.cpp
+++ Project_Sol/main.cpp
@@ -35,22 +35,24 @@ int main(int numArguments, char **arguments)
     CelestialBody &sun = solarSystem.createCelestialBody( vec3(-0.02196846, 0.01239014, 0.00024453), vec3(-1.08324333e-03, 1.50783277e-03, 4.32260022e-05), 1.0, string("Sun"));
 
     // Earth
-    CelestialBody &earth = solarSystem.createCelestialBody( vec3(8.841554736579088E-01, 4.675846666013374E-01, -1.788208627053143E-04),     vec3(-3.03047582e+00, 5.53804088e+00, -2.61593386e-04),   0.000003003, string("Earth") );
+    //CelestialBody &earth = solarSystem.createCelestialBody( vec3(8.841554736579088E-01, 4.675846666013374E-01, -1.788208627053143E-04),     vec3(-3.03047582e+00, 5.53804088e+00, -2.61593386e-04),   0.000003003, string("Earth") );
 
     // Jupiter
-    CelestialBody &jupiter = solarSystem.createCelestialBody( vec3(-5.425134275608231E+00, -4.965390581079263E-01, 1.233903897528774E-01), vec3(0.21935934, -2.61433515, 0.00596296),                0.0009543, string("Jupiter"));
+    //CelestialBody &jupiter = solarSystem.createCelestialBody( vec3(-5.425134275608231E+00, -4.965390581079263E-01, 1.233903897528774E-01), vec3(0.21935934, -2.61433515, 0.00596296),                0.0009543, string("Jupiter"));
 
     // normal mercury
-    CelestialBody &mercury = solarSystem.createCelestialBody( vec3(-3.915735125896518E-01, -8.031632966591709E-02, 2.924535350247253E-02),  vec3(-4.12705352e-03,  -9.60776278e+00, -7.84947463e-01), 1.652e-7, string("Mercury"));
+    //CelestialBody &mercury = solarSystem.createCelestialBody( vec3(-3.915735125896518E-01, -8.031632966591709E-02, 2.924535350247253E-02),  vec3(-4.12705352e-03,  -9.60776278e+00, -7.84947463e-01), 1.652e-7, string("Mercury"));
     // perihelion mercury
-    //CelestialBody &mercury = solarSystem.createCelestialBody( vec3(0.3075, 0, 0),  vec3(0,  12.44, 0), 1.652e-7, string("Mercury"));
+    CelestialBody &mercury = solarSystem.createCelestialBody( vec3(0.3075, 0, 0),  vec3(0,  12.44, 0), 1.652e-7, string("Mercury"));
 
+    /*
     CelestialBody &venus = solarSystem.createCelestialBody(   vec3(3.136264982750417E-01, -6.546683031177923E-01, -2.707665055849010E-02),  vec3(6.63254485, 3.12614069, -0.33993686),                0.000002447, string("Venus"));
     CelestialBody &mars = solarSystem.createCelestialBody(    vec3(1.208658599158087E+00, -6.711650212869039E-01, -4.387238661867205E-02),  vec3(2.69039887, 4.90008363, 0.03660091),                 3.213e-7, string("Mars"));
     CelestialBody &saturn = solarSystem.createCelestialBody(  vec3(-2.236264385979587E+00, -9.782434228347928E+00, 2.590917399206301E-01),  vec3(1.87495851, -0.46042927, -0.06665673),               0.0002857, string("Saturn"));
     CelestialBody &uranus = solarSystem.createCelestialBody(  vec3(1.845409667002747E+01, 7.582158156514705E+00, -2.109158924849341E-01),   vec3(-0.55636872, 1.26177202, 0.011894),                  0.00004365, string("Uranus"));
     CelestialBody &neptune = solarSystem.createCelestialBody( vec3(2.826704082725938E+01, -9.904409112730836E+00, -4.474799704373976E-01),  vec3(0.37149543, 1.0889944, -0.03084886),                 0.0009543, string("Neptune"));
     CelestialBody &pluto = solarSystem.createCelestialBody(   vec3(9.442295163349636E+00, -3.181678067153034E+01, 6.733338703492342E-01),   vec3(1.1266729, 0.09182691, -0.33562222),                 6.58086572e-9, string("Pluto"));
+    */
 
     // The below values are only idealized
     // We don't need to store the reference, but just call the function without a left hand side
@@ -85,12 +87,47 @@ int main(int numArguments, char **arguments)
         for(int timestep=0; timestep<numTimesteps; timestep++) {
             integrator.integrateOneStep(solarSystem);
             solarSystem.writeToFile("positions_verlet_"+string(bodies_string)+".xyz");
-            if (bodies_string == string("SunMercury")){
-
-                solarSystem.writeToFile("");
-            }
         }
         t_stop = clock();
+    }
+    else if (string(integrationmethod) == string("perihelion")){
+        /* Perform verlet method on Sun-Mercury system, looking for the perihelion position
+         * of mercury during every revolution. This position is found
+         * when radial length is smaller then the timestep before and after the
+         * "current" one
+         */
+        double prev_r, r, next_r; //three different radial lengths
+        Verlet integrator(1e-7);
+
+        //selected file for perihelion posiitons only
+        std::ofstream perihelion_file;
+        string perihelion_filename = "perihelion_positions.xyz"; //filename for perihelion positions
+        string data_filename = "positions_data_"+string(bodies_string)+".xyz"; //filename for all positions
+        perihelion_file.open(perihelion_filename.c_str(), ofstream::out);
+
+        prev_r = mercury.position.length(); //establish first radial length
+        //perform one step of integration
+        integrator.integrateOneStep(solarSystem);
+        //solarSystem.writeToFile(data_filename);
+        r = mercury.position.length(); //establish second radial length
+
+        //perform next steps of integration
+        for(int timestep=1; timestep<numTimesteps; timestep++) {
+            vec3 pos_cur = mercury.position;
+            integrator.integrateOneStep(solarSystem);
+            //solarSystem.writeToFile(data_filename);
+            next_r = mercury.position.length(); //establish third radial length
+            if (bodies_string == string("SunMercury")){
+                if (r < prev_r){ //compare radial length to previous value
+                    if (r < next_r) { //compare radial length to next value
+                        perihelion_file << mercury.name << " " << pos_cur.x() << " " << pos_cur.y() << " " << pos_cur.z() << "\n";
+                    }
+                }
+            }
+            prev_r = r;
+            r = next_r;
+        }
+        perihelion_file.close();
     }
     else {
         cout << "You gave the wrong argument, no method specified." << endl
